a4c8e22355e72e2272b2ef80b4755cd982884424
John Broderick <broderick.john.a@gmail.com>
2015-02-28 21:53:19 +0000

    finished game

--- nim.fs
+++ nim.fs
@@ -1,44 +1,70 @@
-module main
+open System
 
-module NimDomain =
+type Player = Player1 | Player2
 
-    type Player = Player1 | Player2
+type Heap = HeapA | HeapB | HeapC
 
-    type Heap = HeapA | HeapB | HeapC
+type GameState = {
+    A : int
+    B : int
+    C : int
+    Playing : Player
+}
 
-    type GameState = {
-        A : int
-        B : int
-        C : int
-        Playing : Player
-    }
+type Move = {
+    heap : Heap
+    take : int
+}
 
-    type Move = {
-        heap : Heap
-        numberToTake : int
-    }
+let from available take =
+    if available - take >= 0
+    then available - take
+    else 0
 
-    let Player1TakesPebbles gs move =
-        match move with
-        | { heap = HeapA; numberToTake = n } -> { gs with A = gs.A - n; Playing = Player2 }
-        | { heap = HeapB; numberToTake = n } -> { gs with B = gs.B - n; Playing = Player2 }
-        | { heap = HeapC; numberToTake = n } -> { gs with C = gs.C - n; Playing = Player2 }
+let Player1TakesPebbles gs move =
+    match move with
+    | { heap = HeapA; take = n } -> { gs with A = (from gs.A n); Playing = Player2 }
+    | { heap = HeapB; take = n } -> { gs with B = (from gs.B n); Playing = Player2 }
+    | { heap = HeapC; take = n } -> { gs with C = (from gs.C n); Playing = Player2 }
 
-    let Player2TakesPebbles gs move =
-        match move with
-        | { heap = HeapA; numberToTake = n } -> { gs with A = gs.A - n; Playing = Player1 }
-        | { heap = HeapB; numberToTake = n } -> { gs with B = gs.B - n; Playing = Player1 }
-        | { heap = HeapC; numberToTake = n } -> { gs with C = gs.C - n; Playing = Player1 }
+let Player2TakesPebbles gs move =
+    match move with
+    | { heap = HeapA; take = n } -> { gs with A = (from gs.A n); Playing = Player1 }
+    | { heap = HeapB; take = n } -> { gs with B = (from gs.B n); Playing = Player1 }
+    | { heap = HeapC; take = n } -> { gs with C = (from gs.C n); Playing = Player1 }
 
-    let rec PlayGame gs move = 
-        match gs with
-        | { A = 0; B = 0;} -> gs
-        | { Playing = Player1 } -> PlayGame (Player1TakesPebbles gs move) move
-        | { Playing = Player2 } -> PlayGame (Player2TakesPebbles gs move) move
+let rec PlayGame gs move = 
+    match gs with
+    | { Playing = Player1 } -> Player1TakesPebbles gs move
+    | { Playing = Player2 } -> Player2TakesPebbles gs move
 
-    let mutable gs = { A = 5; B = 5; C = 5; Playing = Player1 }
-    gs <- PlayGame gs { heap = HeapA; numberToTake = 1; }
+let getHeap (x:String) = 
+    match (x.Split [|' '|]).[0] with
+    | "A" -> HeapA
+    | "B" -> HeapB
+    | "C" -> HeapC
+    | _ -> HeapA
 
-    printfn "%A won" gs.Playing
+let getStones (x:String) = 
+    System.Int32.Parse((x.Split [|' '|]).[1])
 
+let rec gameLoop gs =  
+
+    printfn "A:%i B:%i C:%i" gs.A gs.B gs.C
+
+    printfn "%A to go " gs.Playing
+    
+    let answer = Console.ReadLine()
+    let heap = getHeap answer
+    let stones = getStones answer
+    let nextState = (PlayGame gs { heap = heap; take = stones; })
+
+    match nextState with
+    | { A = 0; B = 0; C = 0 } -> printfn "%A won" gs.Playing
+    | _ -> gameLoop nextState
+
+let mutable gs = { A = 5; B = 5; C = 5; Playing = Player1 }
+gameLoop gs
+
+let x = Console.ReadLine()
 
