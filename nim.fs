b75e0e0865d092ff3cd7c77d3a98ec7f58a75521
John Broderick <broderick.john.a@gmail.com>
2015-03-06 21:06:12 +0000

    various improvements

--- nim.fs
+++ nim.fs
@@ -2,8 +2,6 @@ open System
 
 type Player = Player1 | Player2
 
-type Heap = HeapA | HeapB | HeapC
-
 type GameState = {
     A : int
     B : int
@@ -12,41 +10,28 @@ type GameState = {
 }
 
 type Move = {
-    heap : Heap
+    heap : String
     take : int
 }
 
-let from available take =
+let take available take =
     if available - take >= 0
     then available - take
     else 0
 
-let Player1TakesPebbles gs move =
+let next player = 
+    if player = Player1 then Player2 else Player1
+
+let PlayerTakesPebbles gs move =
     match move with
-    | { heap = HeapA; take = n } -> { gs with A = (from gs.A n); Playing = Player2 }
-    | { heap = HeapB; take = n } -> { gs with B = (from gs.B n); Playing = Player2 }
-    | { heap = HeapC; take = n } -> { gs with C = (from gs.C n); Playing = Player2 }
+    | { heap = "A"; take = n } -> { gs with A = (take gs.A n); Playing = next gs.Playing }
+    | { heap = "B"; take = n } -> { gs with B = (take gs.B n); Playing = next gs.Playing }
+    | { heap = "C"; take = n } -> { gs with C = (take gs.C n); Playing = next gs.Playing }
+    | _ -> gs
 
-let Player2TakesPebbles gs move =
-    match move with
-    | { heap = HeapA; take = n } -> { gs with A = (from gs.A n); Playing = Player1 }
-    | { heap = HeapB; take = n } -> { gs with B = (from gs.B n); Playing = Player1 }
-    | { heap = HeapC; take = n } -> { gs with C = (from gs.C n); Playing = Player1 }
-
-let rec PlayGame gs move = 
-    match gs with
-    | { Playing = Player1 } -> Player1TakesPebbles gs move
-    | { Playing = Player2 } -> Player2TakesPebbles gs move
-
-let getHeap (x:String) = 
-    match (x.Split [|' '|]).[0] with
-    | "A" -> HeapA
-    | "B" -> HeapB
-    | "C" -> HeapC
-    | _ -> HeapA
-
-let getStones (x:String) = 
-    System.Int32.Parse((x.Split [|' '|]).[1])
+let getAnswer (x:String) = 
+    let items = x.Split [|' '|]
+    (items.[0], System.Int32.Parse(items.[1]))
 
 let rec gameLoop gs =  
 
@@ -54,16 +39,14 @@ let rec gameLoop gs =
 
     printfn "%A to go " gs.Playing
     
-    let answer = Console.ReadLine()
-    let heap = getHeap answer
-    let stones = getStones answer
-    let nextState = (PlayGame gs { heap = heap; take = stones; })
+    let heap, stones = getAnswer (Console.ReadLine())
+    let nextState = (PlayerTakesPebbles gs { heap = heap; take = stones; })
 
     match nextState with
     | { A = 0; B = 0; C = 0 } -> printfn "%A won" gs.Playing
     | _ -> gameLoop nextState
 
-let mutable gs = { A = 5; B = 5; C = 5; Playing = Player1 }
+let gs = { A = 5; B = 5; C = 5; Playing = Player1 }
 gameLoop gs
 
 let x = Console.ReadLine()
